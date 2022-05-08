c31a8eeac507bce411f9e05c7299c1873ec12710
RainWolf0603 <yohofam0603@gmail.com>
2022-05-09 02:20:36 +0800

    status

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import json
 import os
 import pathlib
 from discord.ext import commands as cmds
-bot = discord.Bot()
+
+bot = discord.Bot(activity = discord.Activity(type=discord.ActivityType.watching, name="好了啦特哥椅子哥丹利哥死哥維爾戈靈魂收割多佛朗明哥豬大哥蒼藍鴿中華民國國歌UC姐K7姐好運姐冰冰姐美鳳姐小燕姐法拉利姐曾聖傑林俊傑傳奇羅傑端午節中秋節"))
+
 Path = pathlib.Path().absolute()
 
 with open(fr'{Path}/config.json', 'r', encoding='utf8') as jfile:
