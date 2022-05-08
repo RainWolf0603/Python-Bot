6c1b2d6f5b18d62ff7a1ce52dac69d6449da6326
Droppey <coco554211@gmail.com>
2022-05-08 13:21:10 +0800

    Update exchange.py

--- cogs/exchange.py
+++ cogs/exchange.py
@@ -3,11 +3,11 @@ from discord.ext import commands as cmds
 from discord.commands import Option
 import twder
 
-usd = float(twder.now('USD')[1])
-hkd = float(twder.now('HKD')[1])
-eur = float(twder.now('EUR')[1])
-cny = float(twder.now('CNY')[1])
-jpy = float(twder.now('JPY')[1])
+usd = twder.now('USD')
+hkd = twder.now('HKD')
+eur = twder.now('EUR')
+cny = twder.now('CNY')
+jpy = twder.now('JPY')
 
 bot = discord.Bot()
 
@@ -21,6 +21,15 @@ class exchange(cmds.Cog):
         userAvatar = member.avatar
         if 幣值 == 'USD':
             bruh = 金額 * float(usd[1])
+        if 幣值 == 'HKD':
+            bruh = 金額 * float(hkd[1])
+        if 幣值 == 'EUR':
+            bruh = 金額 * float(eur[1])
+        if 幣值 == 'CNY':
+            bruh = 金額 * float(cny[1])
+        if 幣值 == 'JPY':
+            bruh = 金額 * float(jpy[1])
+        
         embed = discord.Embed(title=(f'台幣轉換{幣值}'), description=(f'{金額}') + f'元{幣值}換算新台幣為:{bruh}')
         embed.add_field(name='新台幣:', value=(str(twder.now(f'{幣值}')[1])), inline=True)
         embed.add_field(name="上次更新時間", value=usd[0], inline=False)
